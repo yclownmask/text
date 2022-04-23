f9b648d47b9648b18d7a9d5b6f9b6878502de7cd
yclownmask <93503284+yclownmask@users.noreply.github.com>
2022-04-23 15:55:23 +0800

    Add files via upload

--- 3.c
+++ 3.c
@@ -1,181 +1,268 @@
 #include <stdio.h>
-#include <string.h>
-int main()
+int Sun(int n)
 {
-    char *a[7]={"Gunday","Honday","Fuesday","Eednesday","Chursday","Ariday","Baturday"};
-    char *temp;
-    int i,j;
-    for (i=0;i<6;i++)
-        for (j=0;j<6-i;j++)
-            if (strcmp(a[j],a[j+1])>0)
-            {
-                temp=a[j];
-                a[j]=a[j+1];
-                a[j+1]=temp;
-            }
-    for (i=0;i<7;i++)
-        printf("%s   ",a[i]);
-    printf("\n");
-    return 0;
+    if(n==1) return 1;
+    else return n+Sun(n-1); 
 }
-
+void main()
+{
+    int n;
+    printf("请输入n的值：");
+    scanf("%d",&n);
+    printf("sun n= %d\n",Sun(n));
+}
 
 /*
 #include <stdio.h>
-int main()
+void fun(char *s)
 {
-    int a[2][3]={1,2,3,4,5,6};
-    int b[3][2]={1,1,1,1,1,1},c[2][2]={0,0,0,0};
-    int i,j,k,*p[3];
-    p[0]=a[0];p[1]=b[0];p[2]=c[0];
-    for (i=0;i<2;i++)
-        for (j=0;j<3;j++)
-            for (k=0;k<2;k++)
-                *(p[2]+2*i+k)+=*(p[0]+3*i+j)*(*(p[1]+2*j+k));
-    for (i=0;i<2;i++)
+    char t;
+    if (*s!='\0')
     {
-        for (j=0;j<2;j++)
-            printf("%3d",*(p[2]+2*i+j));
-        printf("\n");
+        t=*s++;
+        fun(s);
+        if (t!='\0') printf("%c",t);
     }
-    return 0;
 }
-
-
-#include <stdio.h>
-int main()
+void main()
 {
-    char *week[]={"Sunday","Monday","Tursday","Wednesday","Thursday","Friday","Saturday"};
-    int n;
-    do
-    {
-        printf("请输入0～6的序号：");
-        scanf("%d",&n);
-    }while(n<=0 || n>=6);
-    printf("英文为：%s\n",week[n]);
-    return 0;
-}
-
-
-#include <stdio.h>
-#include <string.h>
-int main()
-{
-    char *name[6]={"FORTRAN","PASCAL","BASIC","C","COBOL","Smalltalk"};
-    char temp[10],c;
-    int i,j,m,f=0;
-    for (i=0;(c=getchar())!='\n';i++)
-        temp[i]=c;
-    temp[i]='\0';
-    for (i=0;i<6;i++)
-    {
-        for (j=0,m=0;name[i][j]!='\0' && temp[m]!='\0';j++)
-            if (temp[m]==name[i][j])
-                m++;
-        if (temp[m]=='\0')
-        {
-            f=1;
-            printf("%s\n",name[i]);
-        }
-    }
-    if (f==0) printf("没有找到与 %s 相匹配的字符串\n",temp);
-    return 0;
-}
-
-
-#include <stdio.h>
-#include <string.h>
-int main()
-{
-    char *name[6]={"FORTRAN","PASCAL","BASIC","C","COBOL","Amalltalk"};
-    char *temp;
-    int i,j;
-    for (i=0;i<5;i++)
-        for (j=0;j<5-i;j++)
-            if (strcmp(name[j],name[j+1])>0)
-            {
-                temp=name[j+1];
-                name[j+1]=name[j];
-                name[j]=temp;
-            }
-    for (i=0;i<6;i++)
-        printf("%s  ",name[i]);
+    char *a="1234";
+    fun(a);
     printf("\n");
-    return 0;
 }
 
 
-//5-2-57
 #include <stdio.h>
-#include <string.h>
-#define Max 50
-char *maxword(char *s,char *t)
+void fun(int *a,int i,int j)
 {
-    static char comm[Max];
-    char *temp,chs[Max],cht[Max];
-    int i,j,found,maxlen=0;
-    while (*s!='\0')
+    int t;
+    if (i<j)
     {
-        while (*s==' ')
-            s++;
-        for (i=0;*s!='\0' && *s!=' ';i++,s++)
-            chs[i]=*s;
-        chs[i]='\0';
-        if (i>maxlen)
-        {
-            found=0;
-            temp=t;
-            while (*temp!='\0' && found==0)
-            {
-                while (*temp==' ')
-                    temp++;
-                for (j=0;*temp!='\0' && *temp!=' ';j++,temp++)
-                    cht[j]=*temp;
-                cht[j]='\0';
-                if (j==i)
-                {
-                    if (strcmp(chs,cht)==0)
-                    {
-                        maxlen=i;
-                        strcpy(comm,chs);
-                        found=1;
-                    }
-                }
-            }
-        }
+        t=a[i];a[i]=a[j];a[j]=t;
+        fun(a,++i,--j);
     }
-    if (maxlen==0)
-        return NULL;
-    else
-        return comm;
+}
+void main()
+{
+    int a[]={1,2,3,4,5,6},i;
+    fun(a,0,5);
+    for (i=0;i<6;i++)
+        printf("%d ",a[i]);
+    printf("\n");
+}
+
+
+#include <stdio.h>
+char *strc(char a[],char b[],int n)
+{
+    char s[n];
+    int i,j;
+    for (i=0;a[i]!='\0';i++)
+        s[i]=a[i];
+    for (j=0;b[j]!='\0';j++,i++)
+        s[i]=b[j];
+    s[i]='\0';
+    return s;
 }
 int main()
 {
-    char s[Max];
-    char t[Max];
-    char *p;
-    printf("英文句子1: ");
-    gets(s);
-    printf("英文句子2: ");
-    gets(t);
-    p=maxword(s,t);
-    if (p==NULL)
-        printf("没有相同的单词\n");
-    else
-        printf("最长公共单词：%s\n",p);
+    char a[]="hello";
+    char b[]="world";
+    int i,j;
+    for (i=0;a[i]!='\0';i++);
+    for (j=0;b[j]!='\0';j++);
+    printf("%s\n",strc(a,b,i+j));
     return 0;
 }
 
-//5-2-66
+
 #include <stdio.h>
 #include <string.h>
+char *findlong(char *str[],int n,int *length)
+{
+    int max=0;
+    int no,i,tmp;
+    for (i=0;i<n;i++)
+        if((tmp=strlen(str[i]))>max)
+        {
+            max=tmp;
+            no=i;
+        }
+    *length=max;
+    return (str[no]);
+}
+void main()
+{
+    char *str[10],*p;
+    int n,i,length;
+    printf("字符串的个数：");
+    scanf("%d",&n);
+    for (i=0;i<n;i++)
+    {
+        printf("第 %d 个字符串：",i+1);
+        str[i]=(char *)malloc(20);
+        scanf("%s",str[i]);
+    }
+    p=findlong(str,n,&length);
+    printf("最长字符串：%s\t",p);
+    printf("长度：%d\n",length);
+}
+
+
+#include <stdio.h>
+#include <string.h>
+char *maxlen(char *a[])
+{
+    int j;
+    char *max=a[0];
+    for (j=1;j<5;j++)
+        if (strlen(a[j])>strlen(max))
+            max=a[j];
+    return max;
+}
 int main()
 {
-    char s[20];
+    char *a[5]={"hello","hi","tomaro","milek","aaaaaaa"};
+    printf("最长的字符串是：%s\n",maxlen(a));
+    return 0;
+}
+
+
+#include <stdio.h>
+#include <math.h>
+void convert(int *s,char a[],int i)
+{
+    int k,b,j=0;
+    for (k=--i;k>=0;k--,j++)
+    {
+        if (a[k]>='0' && a[k]<='9')
+            b=a[k]-'0';
+        if (a[k]>='A' && a[k]<='F')
+            b=a[k]-'A'+10;
+        *s+=b*pow(16,j);
+    }
+}
+int main()
+{
+    char a[5];
+    int s=0,i;
+    printf("请输入一个十六进制（不超过4位）：");
+    for (i=0;(a[i]=getchar())!='\n';i++);
+    a[i]='\0';
+    convert(&s,a,i);
+    printf("%d\n",s);
+    return 0;
+}
+
+
+#include <stdio.h>
+#include <string.h>
+void f(char p[][10],int n)
+{
+    char t[10];
+    int i,j;
+    for (i=0;i<=n-1;i++)
+        for (j=i+1;j<=n-1;j++)
+            if (strcmp(p[i],p[j])<0)
+            {
+                strcpy(t,p[i]);
+                strcpy(p[i],p[j]);
+                strcpy(p[j],t);
+            }
+}
+int main()
+{
+    char p[][10]={"abc","aabdfg","abbd","dcdbe","cd"};
     int i;
-    printf("请输入一个字符串： ");
-    gets(s);
-    for (i=strlen(s)-1;i>=0;i--)
-        printf("%2c",s[i]);
+    f(p,5);
+    printf("\n%s\n",p[0]);
+    return 0;
+}
+
+
+#include <stdio.h>
+char cchar(char ch)
+{
+    if (ch>='A' && ch<='Z')
+        ch=ch-'A'+'a';
+    return ch;
+}
+int main()
+{
+    char s[]="ABC+abc=defDEF",*p=s;
+    while (*p)
+    {
+        cchar(*p);
+        p++;
+    }
+    printf("%s\n",s);
+}
+
+
+#include <stdio.h>
+int fun()
+{
+    float fen,zfen,aver;
+    int n=0;
+    printf("请输入各个学生的分数：");
+    scanf("%f",&fen);
+    while (fen!=-1)
+    {
+        n++;
+        zfen+=fen;
+        scanf("%f",&fen);
+    }
+    aver=zfen/n;
+    printf("%d 个学生的平均分是 %.2f\n",n,aver);    
+}
+int main()
+{
+    fun();
+    return 0;
+}
+
+
+#include <stdio.h>
+int getbits(short unsigned value,int n1,int n2)
+{
+    short unsigned z;
+    z=~0;
+    z=(z>>n1) & (z<<(16-n2));
+    z=value & z;
+    z=z>>(16-n2);
+    return z;
+}
+int main()
+{
+    short unsigned a;
+    int n1,n2;
+    printf("请输入一个八进制数：");
+    scanf("%ho",&a);
+    printf("n1,n2: ");
+    scanf("%d %d",&n1,&n2);
+    printf("%o\n",getbits(a,n1-1,n2));
+    return 0;
+}
+
+
+#include <stdio.h>
+#include <math.h>
+int func (int n)
+{
+    int i;
+    for (i=2;i<=sqrt(n);i++)
+        if (n%i==0) 
+            return 1;
+    return 0;
+}
+int main()
+{
+    int n;
+    printf("请输入需判断的一个整数：");
+    scanf("%d",&n);
+    if (func(n))
+        printf("%d 不是素数\n",n);
+    else 
+        printf("%d 是素数\n",n);
     return 0;
 }*/
